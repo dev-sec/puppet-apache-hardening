790cb69544a94deacfc7267275a37673b1c039e2
Edmund Haselwanter <me@ehaselwanter.com>
2014-08-06 18:36:55 +0200

    fix puppet lint error. tabs to spaces
    TelekomLabs-DCO-1.1-Signed-off-by: Edmund Haselwanter <me@ehaselwanter.com> (github: ehaselwanter)

--- manifests/apache-hardning.pp
+++ manifests/apache-hardning.pp
@@ -15,44 +15,44 @@
 
 class apache-hardening
 {
-	
-	class { 'apache':
-	  default_mods        => false,
-	  default_confd_files => false,
-	  server_signature => Off,
-	  server_tokens => Prod,
-	  trace_enable => Off,
-	}
-
-
-	#
-	# pure redirecting host from http to https
-	#
-	apache::vhost { 'dtag':
-	  port     => '80',
-	  docroot  => '/var/www/',
-		ssl      => true,
-
-	}
-
-	#
-	# https enabled hosts
-	#
-	apache::vhost { 'dtag-ssl':
-	  port     => '443',
-	  docroot  => '/var/www/',
-		ssl      => true,
-
-		#	
-		#	 configure SSL parameters (ssl cyphers taken from great bettercrypto.org side)
-		#
   
-	  ssl_protocol =>  'All -SSLv2 -SSLv3',
-		ssl_honorcipherorder => On,
-	  ssl_cipher => "EDH+CAMELLIA:EDH+aRSA:EECDH+aRSA+AESGCM:EECDH+aRSA+SHA384:EECDH+aRSA+SHA256:EECDH:+CAMELLIA256:+AES256:+CAMELLIA128:+AES128:+SSLv3:!aNULL:!eNULL:!LOW:!3DES:!MD5:!EXP:!PSK:!DSS:!RC4:!SEED:!ECDSA:CAMELLIA256-SHA:AES256-SHA:CAMELLIA128-SHA:AES128-SHA",
+  class { 'apache':
+    default_mods        => false,
+    default_confd_files => false,
+    server_signature => Off,
+    server_tokens => Prod,
+    trace_enable => Off,
+  }
 
-	}
 
-}	# class apache-hardning
+  #
+  # pure redirecting host from http to https
+  #
+  apache::vhost { 'dtag':
+    port     => '80',
+    docroot  => '/var/www/',
+    ssl      => true,
+
+  }
+
+  #
+  # https enabled hosts
+  #
+  apache::vhost { 'dtag-ssl':
+    port     => '443',
+    docroot  => '/var/www/',
+    ssl      => true,
+
+    # 
+    #  configure SSL parameters (ssl cyphers taken from great bettercrypto.org side)
+    #
+  
+    ssl_protocol =>  'All -SSLv2 -SSLv3',
+    ssl_honorcipherorder => On,
+    ssl_cipher => "EDH+CAMELLIA:EDH+aRSA:EECDH+aRSA+AESGCM:EECDH+aRSA+SHA384:EECDH+aRSA+SHA256:EECDH:+CAMELLIA256:+AES256:+CAMELLIA128:+AES128:+SSLv3:!aNULL:!eNULL:!LOW:!3DES:!MD5:!EXP:!PSK:!DSS:!RC4:!SEED:!ECDSA:CAMELLIA256-SHA:AES256-SHA:CAMELLIA128-SHA:AES128-SHA",
+
+  }
+
+} # class apache-hardning
 
 include apache-hardening

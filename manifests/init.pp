826b86cffb68f1b282a341175fd13109ce71fe3d
Edmund Haselwanter <me@ehaselwanter.com>
2014-09-12 19:34:28 +0200

    add class for apache_hardening and the puppetlabs provider, move testing code to the test-kitchen puppet configuration.
    TelekomLabs-DCO-1.1-Signed-off-by: Edmund Haselwanter <me@ehaselwanter.com> (github: ehaselwanter)

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,52 +7,26 @@
 
 # == Class: apache_hardening
 #
-# Configures apache hardening
+# Configures overlay hardening
 #
+# === Parameters
 #
-# Creates a valid apache2.conf, a virtual SSL host with correct SSL settings
+# [*provider*]
+#   The name of the provider you use to install nginx.
+#   Supported: `puppetlabs/apache`
 #
-
-class apache_hardening
-{
-
-  class { 'apache':
-    default_mods        => false,
-    default_confd_files => false,
-    server_signature    => Off,
-    server_tokens       => Prod,
-    trace_enable        => Off,
+class apache_hardening(
+  $provider = 'none',
+) {
+  case $provider {
+    'puppetlabs/apache': {
+      class{'apache_hardening::puppetlabs': }
+    }
+    'none': {
+      fail('You haven\'t configured a Apache provider for hardening.')
+    }
+    default: {
+      fail('Unrecognized/Unsupported Apache provider for hardening.')
+    }
   }
-
-
-  #
-  # pure redirecting host from http to https
-  #
-  apache::vhost { 'dtag':
-    port     => '80',
-    docroot  => '/var/www/',
-    ssl      => true,
-
-  }
-
-  #
-  # https enabled hosts
-  #
-  apache::vhost { 'dtag-ssl':
-    port                 => '443',
-    docroot              => '/var/www/',
-    ssl                  => true,
-
-    #
-    #  configure SSL parameters (ssl cyphers taken from great bettercrypto.org side)
-    #
-
-    ssl_protocol         =>  'All -SSLv2 -SSLv3',
-    ssl_honorcipherorder => On,
-    ssl_cipher           => 'EDH+CAMELLIA:EDH+aRSA:EECDH+aRSA+AESGCM:EECDH+aRSA+SHA384:EECDH+aRSA+SHA256:EECDH:+CAMELLIA256:+AES256:+CAMELLIA128:+AES128:+SSLv3:!aNULL:!eNULL:!LOW:!3DES:!MD5:!EXP:!PSK:!DSS:!RC4:!SEED:!ECDSA:CAMELLIA256-SHA:AES256-SHA:CAMELLIA128-SHA:AES128-SHA',
-
-  }
-
-} # class apache-hardning
-
-include apache_hardening
+}

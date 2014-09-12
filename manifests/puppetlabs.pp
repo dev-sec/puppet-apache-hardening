04667ad0000058e3997260e52d3fd33ab34c2536
Edmund Haselwanter <me@ehaselwanter.com>
2014-09-12 19:30:54 +0200

    add the first working overlay version. already removed not working/overly complicated class overlay. using parameterized base class instead
    TelekomLabs-DCO-1.1-Signed-off-by: Edmund Haselwanter <me@ehaselwanter.com> (github: ehaselwanter)

---
+++ manifests/puppetlabs.pp
@@ -0,0 +1,36 @@
+# === Copyright
+#
+# Copyright 2014, Deutsche Telekom AG
+# Licensed under the Apache License, Version 2.0 (the "License");
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+# == Class: apache_hardening::apache
+#
+# Overlay provider for puppetlabs/apache
+#
+# === Parameters
+#
+# none
+#
+class apache_hardening::puppetlabs(
+  $allowed_http_methods = [
+    'GET',
+    'POST',
+  ]
+) {
+
+  # make sure our options are written to the config file
+  class{'apache_hardening::puppetlabs_override': }
+
+  # additional configuration
+
+  # addhardening parameters
+
+  $conf_dir = $apache::confd_dir
+
+  file { "${conf_dir}/90.hardening.conf":
+    ensure  => file,
+    content => template('apache_hardening/hardening.conf.erb'),
+  }
+}
